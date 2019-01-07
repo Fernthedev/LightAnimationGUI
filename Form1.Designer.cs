5f6410fe5a7d7102aa486a4cb987dbf8080c4101
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2019-01-07 11:37:03 -0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace LightAnimationGUICsharp
+{
+    partial class LightAnimator4Pi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LightAnimator4Pi));
+            this.selectedpin = new System.Windows.Forms.NumericUpDown();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.frame = new System.Windows.Forms.NumericUpDown();
+            this.fps = new System.Windows.Forms.NumericUpDown();
+            this.whichpi = new System.Windows.Forms.ComboBox();
+            this.onPin = new System.Windows.Forms.Button();
+            this.fpsLabel = new System.Windows.Forms.Label();
+            this.selectedFrameLabel = new System.Windows.Forms.Label();
+            this.pinLabel = new System.Windows.Forms.Label();
+            this.piLabel = new System.Windows.Forms.Label();
+            this.offPin = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.logBox = new System.Windows.Forms.ListView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.savePath = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedpin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fps)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectedpin
+            // 
+            this.selectedpin.AccessibleName = "Pin";
+            this.selectedpin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectedpin.Location = new System.Drawing.Point(668, 40);
+            this.selectedpin.Name = "selectedpin";
+            this.selectedpin.Size = new System.Drawing.Size(120, 20);
+            this.selectedpin.TabIndex = 0;
+            this.selectedpin.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // frame
+            // 
+            this.frame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.frame.Location = new System.Drawing.Point(668, 94);
+            this.frame.Name = "frame";
+            this.frame.Size = new System.Drawing.Size(120, 20);
+            this.frame.TabIndex = 2;
+            this.frame.Tag = "frame";
+            this.frame.ValueChanged += new System.EventHandler(this.frame_ValueChanged);
+            // 
+            // fps
+            // 
+            this.fps.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fps.Location = new System.Drawing.Point(668, 143);
+            this.fps.Name = "fps";
+            this.fps.Size = new System.Drawing.Size(120, 20);
+            this.fps.TabIndex = 3;
+            this.fps.Tag = "fps";
+            this.fps.ValueChanged += new System.EventHandler(this.fps_ValueChanged);
+            // 
+            // whichpi
+            // 
+            this.whichpi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.whichpi.FormattingEnabled = true;
+            this.whichpi.Location = new System.Drawing.Point(668, 196);
+            this.whichpi.Name = "whichpi";
+            this.whichpi.Size = new System.Drawing.Size(121, 21);
+            this.whichpi.TabIndex = 5;
+            this.whichpi.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // onPin
+            // 
+            this.onPin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.onPin.Location = new System.Drawing.Point(12, 415);
+            this.onPin.Name = "onPin";
+            this.onPin.Size = new System.Drawing.Size(75, 23);
+            this.onPin.TabIndex = 6;
+            this.onPin.Text = "On";
+            this.onPin.UseVisualStyleBackColor = true;
+            this.onPin.Click += new System.EventHandler(this.onPin_Click);
+            // 
+            // fpsLabel
+            // 
+            this.fpsLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fpsLabel.AutoSize = true;
+            this.fpsLabel.Location = new System.Drawing.Point(665, 127);
+            this.fpsLabel.Name = "fpsLabel";
+            this.fpsLabel.Size = new System.Drawing.Size(27, 13);
+            this.fpsLabel.TabIndex = 7;
+            this.fpsLabel.Text = "FPS";
+            // 
+            // selectedFrameLabel
+            // 
+            this.selectedFrameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectedFrameLabel.AutoSize = true;
+            this.selectedFrameLabel.Location = new System.Drawing.Point(665, 78);
+            this.selectedFrameLabel.Name = "selectedFrameLabel";
+            this.selectedFrameLabel.Size = new System.Drawing.Size(81, 13);
+            this.selectedFrameLabel.TabIndex = 8;
+            this.selectedFrameLabel.Text = "Selected Frame";
+            // 
+            // pinLabel
+            // 
+            this.pinLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pinLabel.AutoSize = true;
+            this.pinLabel.Location = new System.Drawing.Point(665, 20);
+            this.pinLabel.Name = "pinLabel";
+            this.pinLabel.Size = new System.Drawing.Size(67, 13);
+            this.pinLabel.TabIndex = 9;
+            this.pinLabel.Text = "Selected Pin";
+            // 
+            // piLabel
+            // 
+            this.piLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.piLabel.AutoSize = true;
+            this.piLabel.Location = new System.Drawing.Point(668, 177);
+            this.piLabel.Name = "piLabel";
+            this.piLabel.Size = new System.Drawing.Size(49, 13);
+            this.piLabel.TabIndex = 10;
+            this.piLabel.Text = "Pi Preset";
+            // 
+            // offPin
+            // 
+            this.offPin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.offPin.Location = new System.Drawing.Point(93, 415);
+            this.offPin.Name = "offPin";
+            this.offPin.Size = new System.Drawing.Size(75, 23);
+            this.offPin.TabIndex = 11;
+            this.offPin.Text = "Off";
+            this.offPin.UseVisualStyleBackColor = true;
+            this.offPin.Click += new System.EventHandler(this.offPin_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(22, 399);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(103, 13);
+            this.statusLabel.TabIndex = 12;
+            this.statusLabel.Text = "The current status is";
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(13, 13);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(155, 360);
+            this.logBox.TabIndex = 13;
+            this.logBox.UseCompatibleStateImageBehavior = false;
+            this.logBox.View = System.Windows.Forms.View.List;
+            this.logBox.SelectedIndexChanged += new System.EventHandler(this.logBox_SelectedIndexChanged);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(668, 223);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 14;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // savePath
+            // 
+            this.savePath.Location = new System.Drawing.Point(668, 253);
+            this.savePath.Name = "savePath";
+            this.savePath.Size = new System.Drawing.Size(100, 20);
+            this.savePath.TabIndex = 15;
+            // 
+            // LightAnimator4Pi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.savePath);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.offPin);
+            this.Controls.Add(this.piLabel);
+            this.Controls.Add(this.pinLabel);
+            this.Controls.Add(this.selectedFrameLabel);
+            this.Controls.Add(this.fpsLabel);
+            this.Controls.Add(this.onPin);
+            this.Controls.Add(this.whichpi);
+            this.Controls.Add(this.fps);
+            this.Controls.Add(this.frame);
+            this.Controls.Add(this.selectedpin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LightAnimator4Pi";
+            this.Text = "LightAnimator4Pi";
+            ((System.ComponentModel.ISupportInitialize)(this.selectedpin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fps)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown selectedpin;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.NumericUpDown frame;
+        private System.Windows.Forms.NumericUpDown fps;
+        private System.Windows.Forms.ComboBox whichpi;
+        private System.Windows.Forms.Button onPin;
+        private System.Windows.Forms.Label fpsLabel;
+        private System.Windows.Forms.Label selectedFrameLabel;
+        private System.Windows.Forms.Label pinLabel;
+        private System.Windows.Forms.Label piLabel;
+        private System.Windows.Forms.Button offPin;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.ListView logBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox savePath;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightAnimationGUICsharp
+{
+    public partial class LightAnimator4Pi : Form
+    {
+        private PinData[] pinDatas = new PinData[31];
+        private PinData selectedPinData;
+
+        private int frameInt;
+
+        private List<string> logs = new List<string>();
+
+        public LightAnimator4Pi()
+        {
+            InitializeComponent();
+            //whichpi.Items.Add("custom");
+
+            SupportedPis[] values = (SupportedPis[])Enum.GetValues(typeof(SupportedPis));
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                SupportedPis supported = values[i];
+                whichpi.Items.Add(supported.ToString());
+            }
+
+            for (int i = 0; i < pinDatas.Length; i++)
+            {
+                PinData pinData = new PinData(i);
+                pinDatas[i] = pinData;
+            }
+
+            frameInt = (int)frame.Value;
+
+            selectedPinData = pinDatas[(int)selectedpin.Value];
+
+            selectedPinData.GetFrames().Add(new PinData.FrameData(frameInt, PinData.Pinmode.OFF));
+
+            whichpi.SelectedIndex = 0;
+            fps.Value = 10;
+
+            logBox.Scrollable = true;
+
+
+            statusLabel.Text = "The current status is not set";
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            selectedPinData = pinDatas[(int)selectedpin.Value];
+            updateLog();
+        }
+
+        private void frame_ValueChanged(object sender, EventArgs e)
+        {
+
+            frameInt = (int)frame.Value - 1;
+
+            int frameTime = frameInt + 1;
+
+            frame.Maximum = decimal.MaxValue;
+
+
+
+            if (selectedPinData.GetFrames().ToArray().Length < frameTime)
+            {
+                int times = frameTime - selectedPinData.GetFrames().ToArray().Length;
+
+
+                for (int i = 0; i < times; i++)
+                {
+                    int newFrame = frameTime - i;
+                    selectedPinData.GetFrames().Add(new PinData.FrameData(newFrame, PinData.Pinmode.OFF));
+                }
+
+
+            }
+
+            if (frameInt < 0) frameInt = 0;
+
+            PinData.FrameData frameData = selectedPinData.GetFrames()[frameInt];
+            updateFrame(frameData);
+        }
+
+        private void fps_ValueChanged(object sender, EventArgs e)
+        {
+            fps.Maximum = decimal.MaxValue;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SupportedPis selectedPi = ParseEnum<SupportedPis>(whichpi.SelectedItem.ToString());
+
+            selectedpin.Maximum = AllPins(selectedPi);
+        }
+
+        public static T ParseEnum<T>(string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+        public enum SupportedPis
+        {
+            CUSTOM,
+            //------------------------
+            RaspberryPi_A,
+            RaspberryPi_B_Rev1,
+            RaspberryPi_B_Rev2,
+            RaspberryPi_A_Plus,
+            RaspberryPi_B_Plus,
+            RaspberryPi_ComputeModule,
+            RaspberryPi_2B,
+            RaspberryPi_3B,
+            RaspberryPi_3B_Plus,
+            RaspberryPi_Zero,
+            RaspberryPi_ComputeModule3,
+            RaspberryPi_ZeroW,
+            RaspberryPi_Alpha,
+            RaspberryPi_Unknown,
+            //------------------------
+            // (LEMAKER BANANAPI)
+            BananaPi,
+            BananaPro,
+            //------------------------
+            // (SINOVOIP BANANAPI)  (see: https://github.com/BPI-SINOVOIP/WiringPi/blob/master/wiringPi/wiringPi_bpi.c#L1318)
+            Bpi_M1,
+            Bpi_M1P,
+            Bpi_M2,
+            Bpi_M2P,
+            Bpi_M2P_H2_Plus,
+            Bpi_M2P_H5,
+            Bpi_M2U,
+            Bpi_M2U_V40,
+            Bpi_M2M,
+            Bpi_M3,
+            Bpi_R1,
+            Bpi_M64,
+            //------------------------
+            Odroid,
+            //------------------------
+            OrangePi,
+            //------------------------
+            NanoPi_M1,
+            NanoPi_M1_Plus,
+            NanoPi_M3,
+            NanoPi_NEO,
+            NanoPi_NEO2,
+            NanoPi_NEO2_Plus,
+            NanoPi_NEO_Air,
+            NanoPi_S2,
+            NanoPi_A64,
+            NanoPi_K2
+            //------------------------
+        }
+
+        // *override* static method from subclass
+        // (overriding a static method is not supported in Java
+        //  so this method definition will hide the subclass static method)
+        public int AllPins(SupportedPis supportedPi)
+        {
+            int max;
+            max = 16;
+
+            // no further pins to add for Model B Rev 1 boards
+            if (supportedPi == SupportedPis.RaspberryPi_B_Rev1)
+            {
+                // return pins collection
+                return max;
+            }
+
+            // add pins exclusive to Model A and Model B (Rev2)
+            if (supportedPi == SupportedPis.RaspberryPi_A ||
+               supportedPi == SupportedPis.RaspberryPi_B_Rev2)
+            {
+                max = 20;
+            }
+
+            // add pins exclusive to Models A+, B+, 2B, 3B, and Zero
+            else
+            {
+                max = 31;
+            }
+
+            // return pins collection
+            return max;
+        }
+
+        private void onPin_Click(object sender, EventArgs e)
+        {
+            statusLabel.Text = selectedPinData.GetFrames().ToArray().Length.ToString();
+            PinData.FrameData frameData = selectedPinData.GetFrames()[frameInt];
+
+            updateFrame(frameData, true);
+        }
+
+        private void offPin_Click(object sender, EventArgs e)
+        {
+            PinData.FrameData frameData = selectedPinData.GetFrames()[frameInt];
+
+            updateFrame(frameData, false);
+        }
+
+        private void updateFrame(PinData.FrameData frameData, bool status)
+        {
+            if (status)
+            {
+                frameData.setPinMode(PinData.Pinmode.ON);
+            }
+            else
+            {
+                frameData.setPinMode(PinData.Pinmode.OFF);
+            }
+
+            updateFrame(frameData);
+
+        }
+        private void updateFrame(PinData.FrameData frameData)
+        {
+            if (frameData.GetPinmode().Equals(PinData.Pinmode.ON))
+            {
+                statusLabel.Text = "The current status is on";
+            }
+            else
+            {
+                statusLabel.Text = "The current status is off";
+            }
+            updateLog();
+        }
+
+        public void updateLog()
+        {
+            logBox.Clear();
+
+            for (int i = 0; i < selectedPinData.GetFrames().ToArray().Length; i++)
+            {
+                PinData.FrameData frameData = selectedPinData.GetFrames()[i];
+                logBox.Items.Add("Frame" + frameData.getFrame() + " is " + frameData.GetPinmode().ToString());
+            }
+        }
+
+
+
+        private void save(string filepath)
+        {
+
+
+            foreach (PinData pinData in pinDatas)
+            {
+                List<string> lines = new List<string>();
+                PinData.Pinmode lastPinMode = PinData.Pinmode.OFF;
+                bool run = false;
+
+                bool toSleep = false;
+                decimal sleep = 0;
+
+
+
+                foreach (PinData.FrameData frameData in pinData.GetFrames())
+                {
+
+                    string line = "pin " + pinData.getId() + " " + frameData.GetPinmode().ToString();
+
+                    if (!run)
+                    {
+                        run = true;
+                        lastPinMode = frameData.GetPinmode();
+                        lines.Add(line);
+                        continue;
+                    }
+
+                    if (lastPinMode == frameData.GetPinmode() && run)
+                    {
+                        Console.WriteLine("check 2 " + fps.Value / 1000);
+                        sleep += fps.Value / 1000;
+
+                        toSleep = true;
+                        continue;
+                    }
+
+                    lastPinMode = frameData.GetPinmode();
+
+                    if (toSleep)
+                    {
+                        string oldline = line;
+                        line = "sleep " + sleep;
+                        sleep = 0;
+                        toSleep = false;
+
+                        lines.Add(line);
+                        lines.Add(oldline);
+                        continue;
+                    }
+                    Console.WriteLine("check 5 " + line);
+                    lines.Add(line);
+                }
+
+                if (toSleep)
+                {
+                    string line = "sleep " + sleep;
+                    sleep = 0;
+                    toSleep = false;
+
+                    lines.Add(line);
+                }
+
+
+
+
+               /* Console.WriteLine(lines.ToArray().ToString());
+                logBox.Clear();
+
+                for (int i = 0; i < lines.ToArray().Length; i++)
+                {
+                    logBox.Items.Add(lines[i]);
+                }*/
+
+
+                    using (System.IO.StreamWriter file =
+                        new System.IO.StreamWriter(@filepath + "\\" + pinData.getId().ToString() + ".pia"))
+                    {
+                        foreach (string line in lines)
+                        {
+                            // If the line doesn't contain the word 'Second', write the line to the file.
+
+                            file.WriteLine(line);
+
+                        }
+                    }
+                
+
+
+
+
+            }
+
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            bool openFolderDial = false;
+            if (savePath.Text == "")
+            {
+                openFolderDial = true;
+            }
+            else
+            {
+                try
+                {
+                    Path.GetDirectoryName(savePath.Text);
+                    if(!Directory.Exists(savePath.Text))
+                    {
+                        openFolderDial = true;
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    openFolderDial = true;
+                }
+                catch (PathTooLongException ex)
+                {
+                    openFolderDial = true;
+                }
+
+                if (!openFolderDial)
+                {
+                    save(savePath.Text);
+                }
+            }
+
+            if (openFolderDial && folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                savePath.Text = folderBrowserDialog1.SelectedPath;
+                save(folderBrowserDialog1.SelectedPath);
+            }
+
+        }
+
+        private void logBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (logBox.SelectedItems.Count > 0)
+            {
+                string selected = logBox.SelectedItems[0].Text;
+
+                string result1 = selected.Split(new[] { "Frame" }, StringSplitOptions.None)[1];
+                string result = result1.Split(new[] { " is" }, StringSplitOptions.None)[0];
+
+
+
+                frame.Value = decimal.Parse(result);
+            }
+        }
+
+    }
+
+    public class PinData
+    {
+        private int id;
+        private readonly List<FrameData> frameDatas = new List<FrameData>();
+
+        public PinData(int id)
+        {
+            this.id = id;
+        }
+
+        public List<FrameData> GetFrames()
+        {
+            return frameDatas;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+
+        public class FrameData {
+            private readonly int frame;
+            private Pinmode pinmode;
+
+            public FrameData(int frame, Pinmode pinm)
+            {
+                this.frame = frame;
+                this.pinmode = pinm;
+
+            }
+
+            public Pinmode GetPinmode()
+            {
+                return pinmode;
+            }
+
+            public int getFrame()
+            {
+                return frame;
+            }
+
+            public void setPinMode(Pinmode pinmode)
+            {
+                this.pinmode = pinmode;
+            }
+        }
+
+
+        public enum Pinmode
+        {
+            ON,
+            OFF
+        }
+    }
+}
